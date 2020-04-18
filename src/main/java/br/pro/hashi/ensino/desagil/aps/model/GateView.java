ec71e94169bf118df58aeedbe00617cead280bdb
dualstream799 <dualstream799@gmail.com>
2020-04-17 21:40:16 -0300

    files added

---
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/GateView.java
@@ -0,0 +1,109 @@
+package br.pro.hashi.ensino.desagil.aps.model;
+
+import br.pro.hashi.ensino.desagil.aps.model.Gate;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URL;
+
+public class GateView extends FixedPanel implements ActionListener, MouseListener{
+    private final Gate gate;
+
+    private final JCheckBox input1;
+    private final JCheckBox input2;
+    private final JCheckBox output;
+    private final Image image;
+
+    public GateView(Gate gate) {
+
+        // Como subclasse de FixedPanel, esta classe agora
+        // exige que uma largura e uma altura sejam fixadas.
+        super(245, 346);
+
+        this.gate = gate;
+
+        input1 = new JCheckBox();
+        input2 = new JCheckBox();
+        output = new JCheckBox();
+
+        JLabel inputLabel = new JLabel("Entrada");
+        JLabel outputLabel = new JLabel("Saída");
+
+        // Não há mais a chamada de setLayout, pois ela agora
+        // acontece no construtor da superclasse FixedPanel.
+
+        // Como subclasse de FixedPanel, agora podemos definir a
+        // posição e o tamanho de cada componente ao adicioná-la.
+        add(inputLabel, 10, 10, 75, 25);
+        add(input1, 10, 45, 75, 25);
+        add(input2, 85, 45, 150, 25);
+        add(outputLabel, 10, 311, 75, 25);
+        add(output, 85, 311, 120, 25);
+
+        // Usamos esse carregamento nos Desafios, vocês lembram?
+        String name = gate.toString() + ".png";
+        URL url = getClass().getClassLoader().getResource(name);
+        image = getToolkit().getImage(url);
+
+        // Toda componente Swing tem uma lista de observadores
+        // que reagem quando algum evento de mouse acontece.
+        // Usamos o método addMouseListener para adicionar a
+        // própria componente, ou seja "this", nessa lista.
+        // Só que addMouseListener espera receber um objeto
+        // do tipo MouseListener como parâmetro. É por isso que
+        // adicionamos o "implements MouseListener" lá em cima.
+        addMouseListener(this);
+
+        update();
+    }
+
+    private void update() {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        update();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent event) {
+
+        // Descobre em qual posição o clique ocorreu.
+        int x = event.getX();
+        int y = event.getY();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent event) {
+        // Não precisamos de uma reação específica à ação de pressionar
+        // um botão do mouse, mas o contrato com MouseListener obriga
+        // esse método a existir, então simplesmente deixamos vazio.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent event) {
+        // Não precisamos de uma reação específica à ação de soltar
+        // um botão do mouse, mas o contrato com MouseListener obriga
+        // esse método a existir, então simplesmente deixamos vazio.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent event) {
+        // Não precisamos de uma reação específica à ação do mouse
+        // entrar no painel, mas o contrato com MouseListener obriga
+        // esse método a existir, então simplesmente deixamos vazio.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent event) {
+        // Não precisamos de uma reação específica à ação do mouse
+        // sair do painel, mas o contrato com MouseListener obriga
+        // esse método a existir, então simplesmente deixamos vazio.
+    }
+
+}
